538988d3fa48353fbbf9010b898df3e64229d37a
Antonia Fernandez Figueroa <anto.f.figueroa@gmail.com>
2020-01-02 20:38:06 -0300

    Fixed errors

--- RayOfLight.py
+++ RayOfLight.py
@@ -4,7 +4,9 @@ class RayOfLight:
     def __init__(self, point=None, direction=None):
         if point == None:
             self.P = np.array([0, 0, 0])
-            self.U = np.array([0, 0, 1])
         else:
             self.P = point
+        if direction == None:
+            self.U = np.array([0, 0, 1])
+        else:
             self.U = direction
