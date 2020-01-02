f1ec2f97f8d3200a76d3e53bf3e20c4197081042
Antonia Fernandez Figueroa <anto.f.figueroa@gmail.com>
2020-01-02 20:57:09 -0300

    Modified __init__ methods and attibutes

--- Outflow.py
+++ Outflow.py
@@ -1,5 +1,21 @@
 import numpy as np
+from RayOfLight import *
 
 class Outflow:
-    def __init__(self, D):
-        self.d = np.array([0, 0, 0])
+    def __init__(self, inclination=None, p_a=None, theta=None, V_max=None):
+        if inclination == None:
+            self.i = 0
+        else:
+            self.i = inclination
+        if p_a == None:
+            self.pa = 0
+        else:
+            self.pa = p_a
+        if theta == None:
+            self.theta = np.deg2grad(60)
+        else:
+            self.theta = np.deg2grad(theta)
+        if V_max == None:
+            self.V_max = 100
+        else:
+            self.V_max = V_max
