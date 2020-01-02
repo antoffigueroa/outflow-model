89f58bfcce6a22f8191fcd74734836416fe87327
Antonia Fernandez Figueroa <anto.f.figueroa@gmail.com>
2020-01-02 20:21:46 -0300

    Added __init__ method

--- RayOfLight.py
+++ RayOfLight.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 class RayOfLight:
-    def __init__(self, P, U):
-        self.P = np.array([0, 0, 0])
-        self.U = np.array([0, 0, 1])
+    def __init__(self, point=None, direction=None):
+        if point == None:
+            self.P = np.array([0, 0, 0])
+            self.U = np.array([0, 0, 1])
+        else:
+            self.P = point
+            self.U = direction
