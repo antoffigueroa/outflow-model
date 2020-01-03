31be3dcd395e0bb62a4998b1e3da52b1ea984cde
Antonia Fernandez <anto.f.figueroa@gmail.com>
2020-01-03 14:37:37 -0300

    Created change_pa method

--- Outflow.py
+++ Outflow.py
@@ -23,6 +23,9 @@ class Outflow:
     def change_i(self, inclination):
         self.i = inclination
 
+    def change_pa(self, p_a):
+        self.pa = p_a
+
     def calculate_D(self):
         return np.array([0, 0, 1])
 
