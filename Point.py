430f0312971e8ec19113f2c25e568f91f8a884d4
Antonia Fernandez <anto.f.figueroa@gmail.com>
2020-01-23 11:17:52 -0300

    Added __init__ method for Point

--- Point.py
+++ Point.py
@@ -1,3 +1,12 @@
 import numpy as np
 
 class Point:
+    def __init__(self, coordinates=None, system=None):
+        if coordinates == None:
+            self.coord = np.array([[0, 0, 0]])
+        else:
+            self.coord = coordinates
+        if system == None:
+            self.sys = 'cartesian'
+        else:
+            self.sys = system
