c4a59236ecc226f07f2de4c98229428816a0a10b
Antonia Fernandez <anto.f.figueroa@gmail.com>
2020-01-23 11:27:38 -0300

    Added cart2sphe method

--- Point.py
+++ Point.py
@@ -16,3 +16,28 @@ class Point:
 
     def change_system(self, new_system):
         self.sys = new_system
+    
+    def cart2sphe(self):
+        if self.sys != 'cartesian':
+            print 'ERROR: coordinates are not cartesian'
+            return
+        else:
+            x = self.coord[0][0]
+            y = self.coord[0][1]
+            z = self.coord[0][2]
+            r = np.sqrt(x**2 + y**2 + z**2)
+            if z > 0.:
+                theta = np.arctan((np.sqrt(x**2 + y**2))/z)
+            elif z == 0.:
+                theta = np.pi/2.
+            else:
+                theta = np.arctan((np.sqrt(x**2 + y**2))/z) + np.pi
+            if x > 0:
+                if y >= 0:
+                    phi = np.arctan(y/x)
+                else:
+                    phi = 2*np.pi + np.arctan(y/x)
+            elif x==0:
+                phi = np.pi/2*np.sign(y)
+            else:
+                phi = np.pi + np.arctan(y/x)
